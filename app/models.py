ecae2c8fd19a10b824553fada75ff157c26a036c
Deany256 <gdeandude@gmail.com>
2024-04-06 19:44:22 +0100

    Update models.py
    created a simple ORM

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from . import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, default=0)
+    price = db.Column(db.Float, default=0.0)
+
+    def __repr__(self):
+        return f"<Product {self.name}>"

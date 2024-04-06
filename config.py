a31b4ba81c010a30da800a3f3a81fc9e747927a1
Deany256 <gdeandude@gmail.com>
2024-04-06 19:43:04 +0100

    created config file and running file
    these files are created to simplify making changes and starting the server

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'database', 'inventory.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+

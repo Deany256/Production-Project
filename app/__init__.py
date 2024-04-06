edc3e5926e28de96539a29dfc83adc1266d78dd1
Deany256 <gdeandude@gmail.com>
2024-04-06 19:43:42 +0100

    Update __init__.py
    made it initialise the server

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__, template_folder= "../templates")
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    from .routes import inventory_bp
+    app.register_blueprint(inventory_bp)
+
+    return app

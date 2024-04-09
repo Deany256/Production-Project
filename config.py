ecee83c663161938629cdded0dbb1c099916abb1
Deany256 <gdeandude@gmail.com>
2024-04-09 18:50:28 +0100

    Update config.py
    added super secret key

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'database', 'inventory.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SECRET_KEY = 'super_secret_key'

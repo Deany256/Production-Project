cb23ee36c510e16f140cec437fc445bfbe955f28
Deany256 <gdeandude@gmail.com>
2024-04-12 11:41:16 +0100

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -22,3 +22,8 @@ def test_home_page(client):
     assert response.status_code == 200
     assert b'Welcome to Inventory Management System' in response.data
 
+def test_product_list(client):
+    response = client.get('/products')
+    assert response.status_code == 200
+    assert b'Product List' in response.data
+

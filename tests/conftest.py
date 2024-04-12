da14e6ca52fe73fede24020e4554d4d2e91c5304
Deany256 <gdeandude@gmail.com>
2024-04-12 14:17:17 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,17 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import create_app, db
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from config import TestConfig
 
 @pytest.fixture
 def app():
-    app = create_app()
-    app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app = create_app()  
+    app.config.from_object(TestConfig)
     with app.app_context():
         db.create_all()
         yield app
         db.session.remove()
-        db.drop_all()
+        meta = db.metadata
+        for table in reversed(meta.sorted_tables):
+            db.engine.execute(table.delete())

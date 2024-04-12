75a3c68301303d693604b086723e3ea0ea3d5c0c
Deany256 <gdeandude@gmail.com>
2024-04-12 14:17:08 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'database', 'inventory.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database/inventory.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'super_secret_key'
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    TESTING = True

d52f8d127a76064c67a4d5072ab03079e33d5ffc
Deany256 <gdeandude@gmail.com>
2024-04-17 20:14:39 +0100

    All functionality added

--- app/route.py
+++ app/route.py
@@ -1,6 +1,6 @@
 from secrets import compare_digest
 from dataclasses import dataclass
-from quart import Blueprint, redirect, request, url_for, render_template, g
+from quart import Blueprint, redirect, request, url_for, render_template, g, flash
 from quart_auth import login_required, login_user, logout_user, AuthUser, current_user
 from quart_schema import QuartSchema, validate_request
 
@@ -56,9 +56,50 @@ async def product_details(product_id):
     return await render_template('product_details.html', product=product)
 
 @inventory_bp.route('/add_product', methods=['GET', 'POST'])
-def add_product():
-    pass
+async def add_product():
+    if request.method == 'POST':
+        form = await request.form  # Await the form data coroutine
+        
+        name = form['name']
+        quantity = int(form['quantity'])
+        price = form['price']
+        
+         # Check if quantity is greater than zero
+        if quantity <= 0:
+            flash('Quantity must be greater than zero', 'error')
+            return redirect(request.url)  # Redirect back to the form
+        
+        new_product = await g.connection.execute(f"INSERT INTO product (name, quantity, price) VALUES ('{name}', {quantity}, {price});")
+        
+        
+        return redirect(url_for('inventory.product_list'))
+    
+    return await render_template('add_product.html')
 
 @inventory_bp.route('/products/<int:product_id>/delete', methods=['POST'])
-def delete_product(product_id):
-    pass
+async def delete_product(product_id):
+    product = await g.connection.execute(f"DELETE FROM Product WHERE id = {product_id};")
+    flash('Product deleted successfully!', 'success')
+    return redirect(url_for('inventory.product_list'))
+
+@inventory_bp.route('/products/<int:product_id>/edit', methods=['GET', 'POST'])
+async def edit_product(product_id):
+    product = await g.connection.fetch_all(f"SELECT * FROM product WHERE id = {product_id}")
+
+    if request.method == 'POST':
+        form = await request.form  # Await the form data coroutine
+        
+        name = form['name']
+        quantity = int(form['quantity'])
+        price = form['price']
+
+        if quantity <= 0:
+            flash('Quantity must be greater than zero', 'error')
+            return redirect(request.url)
+
+        await g.connection.execute(f"UPDATE Product SET name = '{name}', quantity = {quantity}, price = {price} WHERE id = {product_id};")
+        
+        flash('Product updated successfully', 'success')
+        return redirect(url_for('inventory.product_list'))
+
+    return await render_template('edit_product.html', product=product)

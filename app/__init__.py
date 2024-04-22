4f917caa13ebdf45fdcff3d8391884607e6b8636
Deany256 <gdeandude@gmail.com>
2024-04-22 06:52:55 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from quart import Quart
-# from quart_sqlalchemy import SQLAlchemy
 from quart_db import QuartDB
 from quart_auth import QuartAuth
 from quart_schema import QuartSchema
@@ -12,25 +11,10 @@ QuartSchema(app)
 db = QuartDB(app,url='sqlite:///database/inventory.db', migrations_folder= 'migrations')
 
 login_manager = QuartAuth()
-
-# db.init_app(app)
-
 login_manager.init_app(app)
 
-# async with app.app_context():
-#     from .models import User
-    
-#     @login_manager.user_loader
-#     async def load_user(user_id):
-#         return await User.query.get(int(user_id))
-    
-#     await db.create_all()
-
-# from .routes import inventory_bp
 from .route import inventory_bp
 app.register_blueprint(inventory_bp)
-
-
 
 def create_app():
     return app
